34ed7993821c06b9483e2e5a434a1bc379975876
Fernando <fa.avalos@uniandes.edu.co>
2022-11-20 22:12:04 -0500

    Pending implementation of how to read numbers and the full numeric stack

--- app/Main.hs
+++ app/Main.hs
@@ -10,49 +10,72 @@ data LispVal = Atom String
             |  Number Integer
             |  String String
             |  Bool Bool
+            |  Character Char
 
--- Auxiliary function to parse escape characters
-parseEscape :: Parser String
-parseEscape =  do
-                backslash <- char '\\'
-                escape <- oneOf "nrt\\\""
-                let value = [backslash] ++ [escape]
-                return value
+-- Task: Parse literals accordng to R5RS standard
+
+parseSpaceLiteral :: Parser Char
+parseSpaceLiteral = do
+    string "space"
+    return ' '
+
+parseNewlineLiteral :: Parser Char
+parseNewlineLiteral = do
+    string "newline"
+    return '\n'
+
+parseLiteral :: Parser LispVal
+parseLiteral = do
+    char '#'
+    char '\\'
+    literal <- (anyChar <|> parseSpaceLiteral <|> parseNewlineLiteral)
+    return $ Character literal
+
+-- Auxiliary function for parsing escape characters
+
+parseEscape :: Parser Char
+parseEscape = do
+    char '\\'
+    escape <- oneOf "nrt\"\\"
+    return (case escape of
+                'n' -> '\n'
+                'r' -> '\r'
+                't' -> '\t'
+                '"' -> '"'
+                '\\' -> '\\')
 
 parseString :: Parser LispVal
 parseString = do
-                char '"'
-                x <- many ((l) <|> parseEscape)
-                char '"'
-                return $ String x
+    char '"'
+    x <- many $ parseEscape <|> anyChar
+    char '"'
+    return $ String x
 
 parseAtom :: Parser LispVal
 parseAtom = do
-                first <- letter <|> symbol
-                rest <- many (letter <|> digit <|> symbol)
-                let atom = first:rest
-                return $ case atom of
-                            "#t" -> Bool True
-                            "#f" -> Bool False
-                            _    -> Atom atom
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+                "#t" -> Bool True
+                "#f" -> Bool False
+                _    -> Atom atom
+
 
 -- Task: Rewrite without liftM but with do-notation and >>= binding
 
 {-
-    I know this is kind of dumb but I just wanted to make sure
-    I understood how monads work at a syntactic and semantic level :p
+    I know this is kind of dumb but I just wanted to make sure I
+    really understood how monads work at a syntactic and semantic
+    level :p
 -}
 
-parseNumber :: Parser LispVal
-parseNumber = do
-                matched <- many1 digit
-                (return matched) >>= (return . Number . read)
-                
+-- Pending: implementation of binary to decimal converter
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
-        <|> parseString
-        <|> parseNumber
+    <|> parseString
+    <|> parseNumber
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
