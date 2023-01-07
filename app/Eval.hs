f8fbd7079be8c63cfdb39dbada759e5d591f9bca
Fernando Avalos <fernandoadev@protonmail.com>
2023-01-07 15:36:16 -0500

    Added Environment support

--- app/Eval.hs
+++ app/Eval.hs
@@ -5,42 +5,43 @@ import Control.Monad.Except ( MonadError(throwError) )
 import LispVal
 import LispError ( ThrowsError, LispError(BadSpecialForm, NotFunction) )
 import LispPrimitive ( primitives )
+import Env
 
-eval :: LispVal -> ThrowsError LispVal
-eval val@(String _)                           = return val
-eval val@(Atom _)                             = return val
-eval val@(Number _)                           = return val
-eval val@(Bool _)                             = return val
-eval val@(Character _)                        = return val
-eval (List [Atom "quote", val])               = return val
-eval (List [Atom "backquote", List vals])     = List <$> mapM evalBackquote vals
-     where evalBackquote (List [Atom "unquote", val]) = eval val
-           evalBackquote val                          = return $ List [Atom "quote", val]
+eval :: Env -> LispVal -> IOThrowsError LispVal
+eval _ val@(String _)                             = return val
+eval _ val@(Atom _)                               = return val
+eval _ val@(Number _)                             = return val
+eval _ val@(Bool _)                               = return val
+eval _ val@(Character _)                          = return val
+eval _ (List [Atom "quote", val])                 = return val
+eval env (List [Atom "backquote", List vals])     = List <$> mapM (evalBackquote env) vals
+     where evalBackquote env (List [Atom "unquote", val]) = eval env val
+           evalBackquote env val                          = return $ List [Atom "quote", val]
 
 {- (DONE)
 Instead of treating any non-false value as true, change the definition 
 of if so that the predicate accepts only Bool values and throws an error 
 on any others.
 -}
-eval (List [Atom "if", pred, conseq, alt]) =
-     do result <- eval pred
+eval env (List [Atom "if", pred, conseq, alt]) =
+     do result <- eval env pred
         case result of
-             Bool True  -> eval conseq
-             Bool False -> eval alt
+             Bool True  -> eval env conseq
+             Bool False -> eval env alt
              badForm    -> throwError $ BadSpecialForm "Unrecognized special form" badForm
 
-eval (List (Atom "cond" : clauses)) = evalCond clauses
-     where evalCond [List [pred@(Bool _), action]]         = eval action
-           evalCond [List [Atom "else", action]]           = eval action
-           evalCond (List [pred, Atom "=>", action] : cs)  = evalCond (List [pred, action] : cs)
-           evalCond (List [pred, action] : cs)             = do result <- eval pred
-                                                                case result of Bool True -> eval action
-                                                                               Bool False -> evalCond cs
-                                                                               badForm    -> throwError $ BadSpecialForm "Unrecognized special form" badForm 
-           evalCond [badForm]                              = throwError $ BadSpecialForm "Unrecognized special form" badForm 
+eval env (List (Atom "cond" : clauses)) = evalCond env clauses
+     where evalCond env [List [pred@(Bool _), action]]         = eval env action
+           evalCond env [List [Atom "else", action]]           = eval env action
+           evalCond env (List [pred, Atom "=>", action] : cs)  = evalCond env (List [pred, action] : cs)
+           evalCond env (List [pred, action] : cs)             = do result <- eval env pred
+                                                                    case result of Bool True -> eval env action
+                                                                                   Bool False -> evalCond env cs
+                                                                                   badForm    -> throwError $ BadSpecialForm "Unrecognized special form" badForm 
+           evalCond env [badForm]                              = throwError $ BadSpecialForm "Unrecognized special form" badForm 
 
-eval (List (Atom func : args))             = mapM eval args >>= apply func
-eval badForm                               = throwError $ BadSpecialForm "Unrecognized special form" badForm
+eval env (List (Atom func : args))             = mapM (eval env) args >>= liftThrows . apply func
+eval _ badForm                               = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func) ($ args) (lookup func primitives)
--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import System.Environment
-import Repl
+import System.Environment ( getArgs )
+import Repl (runOne, runRepl)
 
 main :: IO ()
 main = do args <- getArgs
           case length args of
                 0 -> runRepl
-                1 -> evalAndPrint $ head args
+                1 -> runOne $ head args
                 _ -> putStrLn "Progam takes only 0 or 1 argument"
